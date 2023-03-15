ec36987cf4719544a440dfb684e192d7aa3c76f7
Pingpetrus002 <86714770+Pingpetrus002@users.noreply.github.com>
2023-03-15 11:33:26 +0100

    feat(js): transfert js from index.html to src/main.js

---
+++ src/main.js
@@ -0,0 +1,60 @@
+function verse(){
+
+
+ 
+    const verse1 = document.getElementById("verse1");
+    const verse2 = document.getElementById("verse2");
+    const verse3 = document.getElementById("verse3");
+    const selection = document.getElementById("dropdown");
+    selection.value = '1';
+    
+    verse2.hidden = true;
+    verse3.hidden = true;
+    
+    selection.addEventListener('change', () => {
+          if (selection.value === '1') {
+            verse1.hidden = false;
+            verse2.hidden = true;
+            verse3.hidden = true;
+          } else if(selection.value === '2'){
+              verse2.hidden = false;
+              verse1.hidden = true;
+              verse3.hidden = true;
+            }
+            else if(selection.value === '3'){
+                verse3.hidden = false;
+                verse1.hidden = true;
+                verse2.hidden = true;
+              }
+      })
+
+    }
+    
+    const promise = new Promise((resolve, reject) =>{
+      setTimeout(() => {
+          const randomNumber = Math.floor(Math.random() * 10)
+          if (randomNumber % 2 === 0) {
+              resolve(randomNumber)
+             
+          } else {
+              reject(new Error('Le nombre est impair.'))
+          }
+          
+        
+      }, 1000)
+      
+    })
+    promise 
+        .then(result => console.log(result))
+        .catch(error => console.error(error.message))
+    
+    
+    function fetchSomePokemon(){
+      return fetch('https://pokeapi.co/api/v2/pokemon?limit=200');
+    }
+    
+    const pokemons = fetchSomePokemon();
+    console.log(pokemons)
+
+
+    verse();
